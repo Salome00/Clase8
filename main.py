7055eb79367ee91d575ce3a51d587cd2b06becac
Sara Lloreda Florez <florezsara001@gmail.com>
2023-04-24 11:18:41 -0500

    Ingresa lista de alumnos con sus notas y obtine sus promedios

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import problema1
+from alumnos import alumnos
 
 
 '''
@@ -62,6 +63,10 @@ if __name__ == '__main__':
     print()
 
     problema1.sumar_5_enteros()
+
+    print()
+
+    alumnos()
 
 
 
